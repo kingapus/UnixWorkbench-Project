5d8e16383a4e391f657c46fe25205a2d5be5c7b0
kingapus <kingapus@yahoo.com>
2020-09-29 09:30:10 +0000

    created the working version of guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+files_count=$(ls | wc -l)	#number of files in present directory.
+
+echo "Please guess the number of files in this directory" && echo "Please type your guess and press enter: "	#user input for guessing number of files in directory
+read response
+
+#script generated responses to user input
+
+	while [[ $response -gt $files_count ]]
+	do
+		echo "Your guess is too high. Please try again: "
+		read response
+
+		while [[ $response -lt $files_count ]]
+		do
+			echo "Your guess is too low. Please try again: "
+			read response
+		done
+	done
+
+	while [[ $response -lt $files_count ]]
+	do
+		echo "Your response is too low. Please try again: "
+		read response
+
+		while [[ $response -gt $files_count ]]
+		do
+			echo "Your guess is too high. Please try again: "
+			read response
+		done
+	done
+
+
+	if [[ $response == $files_count ]]
+	then
+		echo "Congratulations!!! Your guess is correct. There are $files_count file(s) in this directory."
+	fi
